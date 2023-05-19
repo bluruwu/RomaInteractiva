7e9de466b8c6b2fe42dced4981b99f6e65cd7f8d
Invitado <invitado@s6pc6.eisc.univalle.edu.co>
2023-05-19 14:57:08 -0500

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
+import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
-  return <Register />;
+  return <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+  </Routes>
 };
 
 export default App;
