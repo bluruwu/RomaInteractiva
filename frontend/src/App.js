b05bf9f57486f34b2bb849686f9d22990344c33f
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-05-18 23:34:52 -0500

    register listo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Login from './components/login/Login';
+import Register from './components/register/Register';
 
 const App = () => {
-  return <Login />;
+  return <Register />;
 };
 
 export default App;
--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -5,17 +5,19 @@ const Login = () => {
     <div className="flex h-screen">
       <div className="flex-1.618 flex justify-center items-center">
         <img
-          src={require('./plaza_san_pedro.jpg')} // Reemplaza "ruta-de-la-imagen.jpg" con la ruta de tu imagen
+          src={require('./coliseo.jpg')} // Reemplaza "ruta-de-la-imagen.jpg" con la ruta de tu imagen
           alt="Imagen de fondo"
           className="block w-full h-full object-cover"
         />
       </div>
       <div className="flex-1 flex flex-col justify-center items-center bg-gray-100 p-40">
         <img
-          src={require('./luperca.jpg')} // Reemplaza "ruta-de-la-imagen.jpg" con la ruta de tu imagen
+          src={require('./spqr.png')} // Reemplaza "ruta-de-la-imagen.jpg" con la ruta de tu imagen
           alt="Imagen en el top right"
-          className="h-max max-w-200 object-cover h-auto max-w-prose"
+          className="h-auto w-auto max-w-30 object-cover "
         />
+        <br/>
+        <br/>
         <div className="mb-6">
           <input
             type="text"
@@ -25,18 +27,30 @@ const Login = () => {
         </div>
         <div className="mb-6">
           <input
+            type="email"
+            className="w-fit px-32 py-4 text-center text-gray-700 rounded-full border border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
+            placeholder="email"
+          />
+        </div>
+        <div className="mb-6">
+          <input
             type="password"
             className="w-fit px-32 py-4 text-center text-gray-700 rounded-full border border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
             placeholder="Password"
           />
         </div>
+        <div className="mb-6">
+          <input
+            type="password"
+            className="w-fit px-32 py-4 text-center text-gray-700 rounded-full border border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
+            placeholder="Repeat Password"
+          />
+        </div>
         <button className="px-6 py-4 text-xl text-white bg-red-500 rounded-full cursor-pointer hover:bg-red-600">
-          Login
+          Register
         </button>
         <br/>
-        <p className="underline ...">Recover password</p>
-        <br/>
-        <p className="underline ...">Don't have an account?</p>
+        <p className="underline ... hover:underline-offset-4 hover:text-green-500 ">Already have an account?</p>
       </div>
     </div>
   );
