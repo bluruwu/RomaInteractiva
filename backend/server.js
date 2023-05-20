fbd29a58d8544a8bdfb371e9ff36b7bff67e72b3
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-05-20 17:58:42 -0500

    form en frontend, conexion con base de datos en backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,79 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 3000; // Puerto en el que escuchará el servidor
+const sql = require('mssql');
 
-// Define las rutas y el manejo de las mismas
-app.get("/", (req, res) => {
-  res.send("¡Hola mundo!");
+// Configuración de la conexión a la base de datos
+const dbConfig = {
+  user: 'edinsonuwu',
+  password: 'Ema12345',
+  server: 'edinsonuwu.database.windows.net',
+  database: 'roma',
+  options: {
+    encrypt: true // Habilita la encriptación
+  }
+};
+
+// Ruta para crear todas las tablas
+app.get('/', async (req, res) => {
+  try {
+    // Conexión a la base de datos
+    await sql.connect(dbConfig);
+
+    // Operación CREATE TABLE de ejemplo
+    const createTableQuery = `
+    DROP TABLE IF EXISTS Usuarios;
+    CREATE TABLE Usuarios(
+        email VARCHAR(100) NOT NULL,
+        nombre_usuario VARCHAR(100) NOT NULL,
+        nombre VARCHAR(100) NOT NULL,
+        contrasena VARCHAR(255) NOT NULL,
+        PRIMARY KEY (email)
+      );
+    `;
+
+    await sql.query(createTableQuery);
+
+    res.send('Tabla creada exitosamente');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error en el servidor');
+  }
 });
 
-// Inicia el servidor
-app.listen(port, () => {
-  console.log(`Servidor Express escuchando en el puerto ${port}`);
+// Ruta para registrar un usuario
+app.post('/usuarios', async (req, res) => {
+  try {
+    // Conexión a la base de datos
+    await sql.connect(dbConfig);
+
+    // Datos del nuevo usuario obtenidos del cuerpo de la solicitud
+    const { email, nombre_usuario, nombre, contrasena } = req.body;
+
+    // Consulta para insertar el nuevo usuario en la tabla
+    const insertQuery = `
+      INSERT INTO Usuarios (email, nombre_usuario, nombre, contrasena)
+      VALUES (@email, @nombre_usuario, @nombre, @contrasena);
+    `;
+
+    // Parámetros de la consulta
+    const parameters = [
+      { name: 'email', value: email },
+      { name: 'nombre_usuario', value: nombre_usuario },
+      { name: 'nombre', value: nombre },
+      { name: 'contrasena', value: contrasena }
+    ];
+
+    // Ejecutar la consulta con los parámetros
+    await sql.query(insertQuery, parameters);
+
+    res.send('Usuario registrado exitosamente');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error en el servidor');
+  }
+});
+
+// Iniciar el servidor
+app.listen(9000, () => {
+  console.log('Servidor Express.js en ejecución');
 });
--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -1,62 +1,88 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
-  const navigate  = useNavigate();
+const Register = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: '',
+    nombre_usuario: '',
+    password: '',
+    repeatPassword: ''
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform form submission logic here
+    console.log(formData);
+  };
+
 
   return (
-    <div className="flex h-screen">
-      <div className="flex-1.618 flex justify-center items-center">
-        <img
-          src={require('./coliseo.jpg')} // Reemplaza "ruta-de-la-imagen.jpg" con la ruta de tu imagen
-          alt="Imagen de fondo"
-          className="block w-full h-full object-cover"
-        />
+    <form onSubmit={handleSubmit}>
+      <div className="flex h-screen">
+        <div className="flex-1.618 flex justify-center items-center">
+          <img
+            src={require('./coliseo.jpg')} // Reemplaza "ruta-de-la-imagen.jpg" con la ruta de tu imagen
+            alt="Imagen de fondo"
+            className="block w-full h-full object-cover"
+          />
+        </div>
+        <div className="flex-1 flex flex-col justify-center items-center bg-gray-100 p-40">
+          <img
+            src={require('./spqr.png')} // Reemplaza "ruta-de-la-imagen.jpg" con la ruta de tu imagen
+            alt="Imagen en el top right"
+            className="max-w-200 max-h-200 object-cover h-auto"
+          />
+          <br />
+          <br />
+          <div className="mb-6">
+            <input
+              type="text"
+              className="w-fit px-32 py-4 text-center text-gray-700 rounded-full border border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
+              placeholder="Username"
+
+              onChange={(e) => setFormData({ ...formData, nombre_usuario: e.target.value })}
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              type="email"
+              className="w-fit px-32 py-4 text-center text-gray-700 rounded-full border border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
+              placeholder="email"
+
+              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              type="password"
+              className="w-fit px-32 py-4 text-center text-gray-700 rounded-full border border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
+              placeholder="Password"
+
+              onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              type="password"
+              className="w-fit px-32 py-4 text-center text-gray-700 rounded-full border border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
+              placeholder="Repeat Password"
+
+              onChange={(e) => setFormData({ ...formData, repeatPassword: e.target.value })}
+            />
+          </div>
+          <button
+            type="submit"
+            className="px-6 py-4 text-xl text-white bg-red-500 rounded-full cursor-pointer hover:bg-red-600">
+            Register
+          </button>
+          <br />
+          <p className="underline ... hover:underline-offset-4 hover:text-green-500 " onClick={() => navigate("/login")}>Already have an account?</p>
+        </div>
       </div>
-      <div className="flex-1 flex flex-col justify-center items-center bg-gray-100 p-40">
-        <img
-          src={require('./spqr.png')} // Reemplaza "ruta-de-la-imagen.jpg" con la ruta de tu imagen
-          alt="Imagen en el top right"
-          className="max-w-200 max-h-200 object-cover h-auto"
-        />
-        <br/>
-        <br/>
-        <div className="mb-6">
-          <input
-            type="text"
-            className="w-fit px-32 py-4 text-center text-gray-700 rounded-full border border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
-            placeholder="Username"
-          />
-        </div>
-        <div className="mb-6">
-          <input
-            type="email"
-            className="w-fit px-32 py-4 text-center text-gray-700 rounded-full border border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
-            placeholder="email"
-          />
-        </div>
-        <div className="mb-6">
-          <input
-            type="password"
-            className="w-fit px-32 py-4 text-center text-gray-700 rounded-full border border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
-            placeholder="Password"
-          />
-        </div>
-        <div className="mb-6">
-          <input
-            type="password"
-            className="w-fit px-32 py-4 text-center text-gray-700 rounded-full border border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
-            placeholder="Repeat Password"
-          />
-        </div>
-        <button className="px-6 py-4 text-xl text-white bg-red-500 rounded-full cursor-pointer hover:bg-red-600">
-          Register
-        </button>
-        <br/>
-        <p className="underline ... hover:underline-offset-4 hover:text-green-500 " onClick={() => navigate("/login")}>Already have an account?</p>
-      </div>
-    </div>
+    </form>
   );
 };
 
-export default Login;
+export default Register;
