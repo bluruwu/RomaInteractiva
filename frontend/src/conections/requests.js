3447cd5b2f7a8320ce794f7bc453a862c125788b
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-05-21 01:16:15 -0500

    Register page, totally finished

--- frontend/src/conections/requests.js
+++ frontend/src/conections/requests.js
@@ -10,14 +10,17 @@ export const postData = async (mydata) => {
   
         if (response.ok) {
           console.log('Data submitted successfully');
+          return 'Data submitted successfully'
           // Perform additional actions after successful submission
         } else {
           const aerror = await response.json()
           console.error(aerror);
+          return 'Email or Username already taken'
           // Handle error response
         }
       } catch (error) {
         console.error('Error:', error);
+        return 'Conection failed'
         // Handle network error
       }
   };
