2965c06b67b2bfc1a38fab900dc41edfb7250bdb
alessandrodiaz <alessdg02@gmail.com>
2023-05-24 10:20:53 -0500

    Arreglo del titulo interfaz de quizes

--- frontend/src/utilities/HomeButton.jsx
+++ frontend/src/utilities/HomeButton.jsx
@@ -3,15 +3,20 @@ import { useNavigate } from "react-router-dom";
 
 const HomeButton = () => {
 	const navigate = useNavigate();
+
+	const handleButtonClick = () => {
+		navigate("/home");
+	};
+
 	return (
-		<div className="mb-4 ">
+		<div className="mb-4">
 			<img
-				src={require("../media/homeicon.png")} // Reemplaza "ruta-de-la-imagen.jpg" con la ruta de tu imagen
+				src={require("../media/homeicon.png")}
 				alt="Imagen de fondo"
-				className="block  object-cover w-11 h-11 cursor-pointer"
-				onClick={() => navigate("/home")}
+				className="block object-cover w-11 h-11 cursor-pointer"
+				onClick={handleButtonClick}
 			/>
-			<p className=" font-bold cursor-pointer" onClick={() => navigate("/home")}>
+			<p className="font-bold cursor-pointer" onClick={handleButtonClick}>
 				Home
 			</p>
 		</div>
