7939f61bced94b829f367be3dfdcdc1207cc1bb1
alessandrodiaz <alessdg02@gmail.com>
2023-05-26 22:31:09 -0500

    Registro correccion

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,170 +2,160 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { postData } from "../conections/requests";
 import HomeButton from "../utilities/HomeButton";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 const Register = () => {
-  const navigate = useNavigate();
-  //data that is going to be sent to the backend
-  const [formData, setFormData] = useState({
-    email: "",
-    nombre_usuario: "",
-    nickname: "",
-    contrasena: "",
-  });
+	const navigate = useNavigate();
+	//data that is going to be sent to the backend
+	const [formData, setFormData] = useState({
+		email: "",
+		nombre_usuario: "",
+		nickname: "",
+		contrasena: "",
+	});
 
-  const [confirmedPassword, setConfirmedPassword] = useState();
+	const [confirmedPassword, setConfirmedPassword] = useState();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Perform form submission logic here
-    console.log(formData);
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		// Perform form submission logic here
+		console.log(formData);
 
-    if (formData["contrasena"].length < 8) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Password is too short',
-      })
-      return;
-    }
+		if (formData["contrasena"].length < 8) {
+			Swal.fire({
+				icon: "error",
+				title: "Oops...",
+				text: "Password is too short",
+			});
+			return;
+		}
 
-    if (formData["contrasena"] != confirmedPassword) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Passwords are not the same',
-      })
-      return;
-    }
+		if (formData["contrasena"] != confirmedPassword) {
+			Swal.fire({
+				icon: "error",
+				title: "Oops...",
+				text: "Passwords are not the same",
+			});
+			return;
+		}
 
-    if (formData["nombre_usuario"].length < 8) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Username is too short',
-      })
-      return;
-    }
+		if (formData["nombre_usuario"].length < 8) {
+			Swal.fire({
+				icon: "error",
+				title: "Oops...",
+				text: "Username is too short",
+			});
+			return;
+		}
 
-    if (formData["nickname"].length < 5) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Nickname is too short',
-      })
-      return;
-    }
+		if (formData["nickname"].length < 5) {
+			Swal.fire({
+				icon: "error",
+				title: "Oops...",
+				text: "Nickname is too short",
+			});
+			return;
+		}
 
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formData["email"]) == false) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Email is not valid',
-      })
-      return;
-    }
+		if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formData["email"]) == false) {
+			Swal.fire({
+				icon: "error",
+				title: "Oops...",
+				text: "Email is not valid",
+			});
+			return;
+		}
 
-    const myresponse = async () => {
-      const req_succesful = await postData(formData);
-      if (req_succesful === "Data submitted successfully") {
-        Swal.fire(
-          'Congrats!',
-          'You have succesfully been register!',
-          'success'
-        )
-        //alert("Register succesful");
-        navigate("/login");
-      } else {
-        Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: 'Email or username already taken',
-        })
-      }
-    };
-    myresponse();
-  };
+		const myresponse = async () => {
+			const req_succesful = await postData(formData);
+			if (req_succesful === "Data submitted successfully") {
+				Swal.fire("Congrats!", "You have succesfully been register!", "success");
+				//alert("Register succesful");
+				navigate("/login");
+			} else {
+				Swal.fire({
+					icon: "error",
+					title: "Oops...",
+					text: "Email or username already taken",
+				});
+			}
+		};
+		myresponse();
+	};
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="flex h-screen">
-        <div className="flex-1.618 flex justify-center items-centers">
-          <img
-            src={require("../media/coliseo.jpg")}
-            alt="Imagen de fondo"
-            className="block w-full h-full object-cover"
-          />
-        </div>
-        <div className="font-text flex-1 flex flex-col justify-center items-center bg-gray-100 p-20 w-2/3">
-          <div className="w-full flex justify-end">
-            <HomeButton />
-          </div>
-          <img
-            src={require("../media/spqr.png")}
-            alt="Imagen en el top right"
-            className="max-w-200 max-h-200 object-cover h-auto"
-          />
-          <br />
-          <br />
-          <div className="mb-4">
-            <input
-              type="text"
-              className="w-fit px-32 py-2 text-center text-gray-700 rounded-full border-4 border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
-              placeholder="Full name"
-              onChange={(e) => setFormData({ ...formData, nombre_usuario: e.target.value })}
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              type="text"
-              className="w-fit px-32 py-2 text-center text-gray-700 rounded-full border-4 border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
-              placeholder="Nickname"
-              onChange={(e) => setFormData({ ...formData, nickname: e.target.value })}
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              type="email"
-              className="w-fit px-32 py-2 text-center text-gray-700 rounded-full border-4 border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
-              placeholder="Email"
-              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              type="password"
-              className="w-fit px-32 py-2 text-center text-gray-700 rounded-full border-4 border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
-              placeholder="Password"
-              onChange={(e) => setFormData({ ...formData, contrasena: e.target.value })}
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              type="password"
-              className="w-fit px-32 py-2 text-center text-gray-700 rounded-full border-4 border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400"
-              placeholder="Repeat Password"
-              onChange={(e) => setConfirmedPassword(e.target.value)}
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full py-2 text-xl text-white bg-red-500 rounded-full cursor-pointer hover:bg-red-600"
-            style={{ backgroundColor: "rgb(142, 0, 0)" }}
-          >
-            Register
-          </button>
-          <br />
-          <p
-            className="underline ... hover:underline-offset-4 hover:text-green-500 "
-            onClick={() => navigate("/login")}
-          >
-            Already have an account?
-          </p>
-        </div>
-      </div>
-    </form>
-  );
+	return (
+		<form onSubmit={handleSubmit}>
+			<div className="flex h-screen">
+				{/* Imagen de fondo */}
+				<div className=" flex justify-center items-centers">
+					<img
+						src={require("../media/coliseo.jpg")}
+						alt="Imagen de fondo"
+						className="block w-full h-full object-cover"
+					/>
+				</div>
+
+				{/* Panel derecho */}
+				<div className="font-text flex-1 flex flex-col justify-center items-center bg-gray-100 p-16">
+					{/* HomeButton */}
+					<div className="w-full flex justify-end">
+						<HomeButton />
+					</div>
+
+					{/* Logo */}
+					<img src={require("../media/logo.png")} alt="Imagen en el top right" className="h-42" />
+
+					{/* Formulario */}
+					<div className="flex flex-col items-center mb-12">
+						<input
+							type="text"
+							className="w-fit px-28 py-2 text-center text-gray-700 rounded-full border-4 border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400 mb-2"
+							placeholder="Full name"
+							onChange={(e) => setFormData({ ...formData, nombre_usuario: e.target.value })}
+						/>
+						<input
+							type="text"
+							className="w-fit px-28 py-2 text-center text-gray-700 rounded-full border-4 border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400 mb-2"
+							placeholder="Nickname"
+							onChange={(e) => setFormData({ ...formData, nickname: e.target.value })}
+						/>
+						<input
+							type="email"
+							className="w-fit px-28 py-2 text-center text-gray-700 rounded-full border-4 border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400 mb-2"
+							placeholder="Email"
+							onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+						/>
+						<input
+							type="password"
+							className="w-fit px-28 py-2 text-center text-gray-700 rounded-full border-4 border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400 mb-2"
+							placeholder="Password"
+							onChange={(e) => setFormData({ ...formData, contrasena: e.target.value })}
+						/>
+						<input
+							type="password"
+							className="w-fit px-28 py-2 text-center text-gray-700 rounded-full border-4 border-gray-300 focus:outline-none focus:border-red-500 placeholder-gray-400 mb-2"
+							placeholder="Repeat Password"
+							onChange={(e) => setConfirmedPassword(e.target.value)}
+						/>
+						<button
+							type="submit"
+							className="w-full py-2 text-xl text-white bg-red-500 rounded-full cursor-pointer hover:bg-red-600"
+							style={{ backgroundColor: "rgb(142, 0, 0)" }}
+						>
+							Register
+						</button>
+						<br />
+						<p
+							className="underline ... hover:underline-offset-4 hover:text-green-500 "
+							onClick={() => navigate("/login")}
+						>
+							Already have an account?
+						</p>
+					</div>
+				</div>
+			</div>
+		</form>
+	);
 };
 
 export default Register;
