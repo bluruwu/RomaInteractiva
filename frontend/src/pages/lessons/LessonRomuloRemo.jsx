00407b5a0ac6d2c53d0acb5f8153d971a9e5c3ad
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-06-09 08:15:53 -0500

    historia 38 finalizada

--- frontend/src/pages/lessons/LessonRomuloRemo.jsx
+++ frontend/src/pages/lessons/LessonRomuloRemo.jsx
@@ -4,11 +4,11 @@ import LessonText from "../../components/lesson/LessonText";
 import LessonModel from "../../components/lesson/LessonModel";
 import LessonNav from "../../components/lesson/LessonNav";
 
-const first = ``
+const first = `Rómulo y Remo son figuras legendarias en la mitología romana y desempeñaron un papel crucial en la fundación de la antigua ciudad de Roma. Según la historia, eran hijos de Rea Silvia, una princesa descendiente de Eneas, y fueron abandonados en las orillas del río Tíber. Allí, fueron amamantados y cuidados por una loba, hasta que fueron encontrados por un pastor llamado Faustulo, quien se hizo cargo de su crianza.`
 
-const second = ``
+const second = `A medida que Rómulo y Remo crecían, se convirtieron en hombres fuertes y valientes. Sin embargo, surgieron conflictos entre ellos sobre quién debería ser el fundador de la ciudad que deseaban establecer en la región. Para resolver la disputa, acordaron buscar un augurio divino. Rómulo observó una serie de aves de presa, mientras que Remo vio un grupo de aves más tarde. Interpretando esto como una señal de los dioses, Rómulo fue declarado como el elegido para fundar la ciudad.`
 
-const third = ``
+const third = `Sin embargo, la rivalidad entre los hermanos no se resolvió fácilmente. En un acto impulsivo, Rómulo construyó los muros de la nueva ciudad y, en un momento de enojo, mató a Remo por saltar por encima de ellos. Aunque este episodio trágico ensombreció su relación, Rómulo siguió adelante y bautizó a la ciudad con su propio nombre: Roma, que se convertiría en el epicentro de un vasto imperio que duraría siglos.`
 
 const LessonRomuloRemo = () => {
 
@@ -25,7 +25,7 @@ const LessonRomuloRemo = () => {
 					thirdparag={third}
 				/> 
 				{/* Componente del modelo 3D */}
-				<LessonModel description="" titleModel="" source=""/>
+				<LessonModel description="Rómulo y Remo de Pequeños" titleModel="Rómulo y Remo" source="https://sketchfab.com/models/23ad914573fe46c49b510a736715a377/embed"/>
 			</div>
 			{/* Navegación entre lecciones */}
 			<LessonNav urlbef="/Final_Imperio" mediabef="finalimperio.jpg" titlebef="FINAL DEL IMPERIO ROMANO" 
