b368e02c6dd1b6ff1cc9ca186761cec0f8d32e98
David Moreno <davidan7845@gmail.com>
2023-06-09 19:40:50 -0500

    se implemento la leccion del colapso de la republica

--- frontend/src/pages/lessons/LessonColapsoRepublica.jsx
+++ frontend/src/pages/lessons/LessonColapsoRepublica.jsx
@@ -4,11 +4,9 @@ import LessonText from "../../components/lesson/LessonText";
 import LessonModel from "../../components/lesson/LessonModel";
 import LessonNav from "../../components/lesson/LessonNav";
 
-const first = ``
+const first = `A medida que Roma conquistaba nuevos territorios, surgieron tensiones sociales y económicas, ya que las élites romanas se beneficiaban de la expansión mientras que las clases bajas sufrían las consecuencias. Esto condujo a una creciente polarización social y a un aumento de la corrupción política.`
 
-const second = ``
-
-const third = ``
+const second = `La crisis política también se vio exacerbada por líderes ambiciosos que aprovecharon las tensiones existentes para obtener más poder. Generales como Julio César y Pompeyo establecieron ejércitos privados y se involucraron en una serie de guerras civiles que socavaron aún más la estructura republicana. Finalmente, en el 27 a.C., Octavio, más tarde conocido como Augusto, se convirtió en el primer emperador romano, poniendo fin oficialmente a la República Romana y estableciendo el Imperio Romano.`
 
 const LessonColapsoRepublica = () => {
 
@@ -22,9 +20,8 @@ const LessonColapsoRepublica = () => {
 					title="Colapso de la República Romana" 
 					firstparag = {first} 
 					secondparag={second} 
-					thirdparag={third}
 				/> 
-				<LessonModel description="" titleModel="" source=""/>
+				<LessonModel description="Ciudad Roma" titleModel="Roma" source="https://sketchfab.com/models/0304c6618b984328a6829d474b6e87b4/embed"/>
 			</div>
 			{/* Navegación entre lecciones */}
 			<LessonNav urlbef="/Expansion_Republica" mediabef="expansion.jpg" titlebef="EXPANSIÓN DE LA REPÚBLICA ROMANA" 
