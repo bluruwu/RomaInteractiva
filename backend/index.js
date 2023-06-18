a06109931f0c4712266102a07570b4f981235911
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-06-18 17:37:31 -0500

    arreglo de back-cors

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
-const corsOptions = {
-    origin: ['*'],
-    allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Request-Headers"],
-    credentials: true,
-    enablePreflight: true
-}
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions))
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();})
 
 
 //Credenciales supabase
