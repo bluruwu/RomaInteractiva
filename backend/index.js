e5ec21a54c3e67e53735f197af4cbc2e43224172
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-06-18 14:40:45 -0500

    arreglando el deploy

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,13 @@ app.use(bodyParser.json());
 // allow all the incoming ip
 app.use(cors());
 
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	next();
+  })
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 //POST para el inicio de sesion de los usuarios
