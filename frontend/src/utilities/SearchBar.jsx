bc9bcb83bcfc3ca0cd1bbdc890e7d24188966767
@Bluruwu <juandiegotres@hotmail.com>
2023-06-18 21:25:00 -0500

    Busqueda al azar añadida

--- frontend/src/utilities/SearchBar.jsx
+++ frontend/src/utilities/SearchBar.jsx
@@ -2,23 +2,13 @@ import React, { useState } from "react";
 import Fuse from "fuse.js";
 import { Link } from "react-router-dom";
 import lupa from "../media/iconos/lupa.png";
+import lessons from "./lessonsPath";
 
 const SearchBar = () => {
 	// Las funciones de abajo son para la busqueda. Lessons son las lecciones que se añadiran en la busqueda, y keys es aquello que se va a buscar
 	// Si se quiere colocar más keys, primero agrega a lessons aquello por lo que se va a buscar y luego lo incluyes en keys.
 
 	const [searchTerm, setSearchTerm] = useState("");
-
-	const lessons = [
-		{ title: "Fundación de Roma", path: "/Fundacion_de_Roma" },
-		{ title: "Reyes de Roma", path: "/Reyes_de_Roma" },
-		{ title: "Finalización de la monarquía", path: "/Final_Monarquia" },
-		{ title: "Augusto", path: "/Augusto" },
-		{ title: "Rómulo y Remo", path: "/Romulo_Remo" },
-		{ title: "Fundación de la Republica Romana", path: "/Fundacion_Republica" },
-		{ title: "Expansión de la Republica Romana", path: "/Expansion_Republica" },
-		{ title: "Colapso de la República Romana", path: "/Colapso_Republica" },
-	];
 
 	const fuse = new Fuse(lessons, {
 		keys: ["title"],
@@ -32,16 +22,17 @@ const SearchBar = () => {
 	return (
 		<>
 			<form>
-				<div className="flex items-center">
+				<div className="flex items-center py-4">
 					<input
 						type="text"
 						value={searchTerm}
 						onChange={(event) => setSearchTerm(event.target.value)}
-						className="px-4 py-2 border border-gray-300 rounded-l-full focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
+						className="px-4 py-2 w-56 border border-gray-300 rounded-l-full focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
 						id="busqueda"
+						aria-labelledby="search"
 					/>
-					<button type="submit" className="px-4 py-3 bg-white text-black rounded-r-full">
-						<img src={lupa} alt="Lupa" className="w-5 h-auto"></img>
+					<button type="submit" className="px-4 py-3 min-w-[52px] min-h-[41px] bg-white text-black rounded-r-full">
+						<img src={lupa} alt="Lupa" className="w-5 min-w-[11px]"></img>
 					</button>
 				</div>
 			</form>
