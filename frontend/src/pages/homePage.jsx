a63a9aae684a1bd293df00ff02220a7b5993a017
alessandrodiaz <alessdg02@gmail.com>
2023-06-22 18:22:21 -0500

    Correccion en las calificaciones en el home

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -16,30 +16,37 @@ const HomePage = () => {
 	const token = localStorage.getItem("token");
 	const [scoreData, setScoreData] = useState([]);
 
+	//Poner las calificaciones en la parte superior derecha de los botones
 	useEffect(() => {
+		//Obtener la calificacion de las lecciones del localstorage
+		const getScoreDataFromLocalStorage = () => {
+			return [
+				{
+					leccion: 1,
+					nombre: "monarquia",
+					nota: localStorage.getItem("monarquiaAciertos"),
+				},
+				{
+					leccion: 2,
+					nombre: "republica",
+					nota: localStorage.getItem("republicaAciertos"),
+				},
+				{
+					leccion: 4,
+					nombre: "personajes",
+					nota: localStorage.getItem("personajesAciertos"),
+				},
+				// Agregar más elementos según sea necesario para las demás lecciones
+			];
+		};
+
+		//Si el usuario esta logeado, hacer la peticion de las calificaciones al backend
 		if (token) {
 			async function fetchCalificaciones() {
 				const result = await getCalificaciones(token);
 				if (result === "OK") {
-					// Actualizar los valores de número utilizando el estado local
-					setScoreData([
-						{
-							leccion: 0,
-							nombre: "monarquia",
-							nota: localStorage.getItem("monarquiaAciertos"),
-						},
-						{
-							leccion: 1,
-							nombre: "republica",
-							nota: localStorage.getItem("republicaAciertos"),
-						},
-						{
-							leccion: 4,
-							nombre: "personajes",
-							nota: localStorage.getItem("personajesAciertos"),
-						},
-						// Agregar más elementos según sea necesario para las demás lecciones
-					]);
+					const calificaciones = getScoreDataFromLocalStorage();
+					setScoreData(calificaciones);
 				} else {
 					console.log("No se pudo obtener las calificaciones");
 				}
@@ -47,7 +54,10 @@ const HomePage = () => {
 
 			fetchCalificaciones();
 		} else {
+			//Si el usuario NO esta logeado, obtener las calificaciones del localstorage
 			console.log("Usuario no autenticado");
+			const calificaciones = getScoreDataFromLocalStorage();
+			setScoreData(calificaciones);
 		}
 
 		// Verificar si la variable ya existe en el almacenamiento local
@@ -87,9 +97,9 @@ const HomePage = () => {
 	const getScore = (leccion) => {
 		const scoreDataItem = scoreData.find((item) => item.leccion === leccion);
 		if (scoreDataItem) {
-			return scoreDataItem.nota || 0;
+			return scoreDataItem.nota || "-";
 		} else {
-			return 0;
+			return "-";
 		}
 	};
 
@@ -110,7 +120,7 @@ const HomePage = () => {
 							hoverImage={monarquiaImage}
 							onClick={() => navigate("/fundacion_de_roma")}
 							buttonText={"La Monarquía"}
-							number={getScore(0)}
+							number={getScore(1)}
 						/>
 						<Button
 							id="republica"
@@ -118,7 +128,7 @@ const HomePage = () => {
 							hoverImage={republicaImage}
 							onClick={() => navigate("/Fundacion_Republica")}
 							buttonText={"La República"}
-							number={getScore(1)}
+							number={getScore(2)}
 						/>
 						<Button
 							id="imperio"
@@ -126,7 +136,7 @@ const HomePage = () => {
 							hoverImage={imperioImage}
 							onClick={() => navigate("/Cristianismo_Imperio")}
 							buttonText={"El Imperio"}
-							number={0}
+							number={getScore(3)}
 						/>
 					</div>
 				</section>
@@ -149,7 +159,7 @@ const HomePage = () => {
 							onClick={() => navigate("/Coliseo_Romano")}
 							hoverImage={coliseoImage}
 							buttonText={"Arquitectura"}
-							number={0}
+							number={getScore(5)}
 						/>
 						<Button
 							id="cultura"
@@ -157,7 +167,7 @@ const HomePage = () => {
 							hoverImage={domusImage}
 							onClick={() => navigate("/Viviendas")}
 							buttonText={"Cultura"}
-							number={0}
+							number={getScore(6)}
 						/>
 					</div>
 				</section>
