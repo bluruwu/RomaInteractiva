2da25695e82e2984c6c74ee793e8a873a27bef3e
alessandrodiaz <alessdg02@gmail.com>
2023-06-23 08:54:57 -0500

    Guardar calificacion de quiz del imperio en la base de datos y otras correcciones

--- frontend/src/utilities/imperioInfo.js
+++ frontend/src/utilities/imperioInfo.js
@@ -1,44 +1,53 @@
 export const INFORMATION = [
-    { //0
-        title: " ¿Cuál fue el cambio más significativo para el cristianismo en Roma en el siglo IV d.C.?",
-        option1: "La construcción de magníficas iglesias",
-        option2: "El emperador Constantino e vuelve cristiano",
-        option3: "Promulgación del Edicto de Milán en el año 313",
-        option4: "La tolerancia religiosa",
-        respuesta: 3,
-        urlbef: "/Cristianismo_Imperio"
-    },
-    { //1
-        title: "¿Cuál era el arma principal de los soldados romanos en el combate cuerpo a cuerpo?",
-        option1: "Espada gladius",
-        option2: "Lanza pilum",
-        option3: "Catapulta",
-        option4: "Balista",
-        respuesta: 1
-    },
-    { //2
-        title: "¿Cuál fue uno de los factores clave que contribuyó al colapso gradual del Imperio Romano?",
-        option1: "La expansión del Imperio Romano de Oriente",
-        option2: "La política en el Imperio Romano de Occidente",
-        option3: "Florecimiento económico del Imperio Romano",
-        option4: "La intervención de los pueblos asiáticos",
-        respuesta: 2
-    },
-    { //3
-        title: "¿Cuándo fue depuesto el último emperador romano de Occidente, marcando el fin simbólico del Imperio Romano de Occidente?",
-        option1: "476 d.C.",
-        option2: "313 d.C.",
-        option3: "1453 d.C.",
-        option4: "732 d.C.​",
-        respuesta: 1
-    },  
-    { //4
-        title: "¿Qué papel desempeñaron los líderes cristianos en la vida política y social de la ciudad de Roma?",
-        option1: "Construir magníficas iglesias y basílicas",
-        option2: "Ser objeto de persecuciones periódicas",
-        option3: "Ser considerados una amenaza para el orden",
-        option4: "Tener un rol destacado en la política y sociedad",
-        respuesta: 4,
-        urlnxt: "/home"
-    }, 
-]
+	{
+		//0
+		title:
+			"¿Cuál fue el cambio más significativo para el cristianismo en Roma en el siglo IV d.C.?",
+		option1: "La construcción de magníficas iglesias",
+		option2: "El emperador Constantino se vuelve cristiano",
+		option3: "Promulgación del Edicto de Milán en el año 313",
+		option4: "La tolerancia religiosa",
+		respuesta: 3,
+		urlbef: "/Final_Imperio",
+	},
+	{
+		//1
+		title: "¿Cuál era el arma principal de los soldados romanos en el combate cuerpo a cuerpo?",
+		option1: "Espada gladius",
+		option2: "Lanza pilum",
+		option3: "Catapulta",
+		option4: "Balista",
+		respuesta: 1,
+	},
+	{
+		//2
+		title:
+			"¿Cuál fue uno de los factores clave que contribuyó al colapso gradual del Imperio Romano?",
+		option1: "La expansión del Imperio Romano de Oriente",
+		option2: "La política en el Imperio Romano de Occidente",
+		option3: "Florecimiento económico del Imperio Romano",
+		option4: "La intervención de los pueblos asiáticos",
+		respuesta: 2,
+	},
+	{
+		//3
+		title:
+			"¿Cuándo fue depuesto el último emperador romano de Occidente, marcando el fin simbólico del Imperio Romano de Occidente?",
+		option1: "476 d.C.",
+		option2: "313 d.C.",
+		option3: "1453 d.C.",
+		option4: "732 d.C.​",
+		respuesta: 1,
+	},
+	{
+		//4
+		title:
+			"¿Qué papel desempeñaron los líderes cristianos en la vida política y social de la ciudad de Roma?",
+		option1: "Construir magníficas iglesias y basílicas",
+		option2: "Ser objeto de persecuciones periódicas",
+		option3: "Ser considerados una amenaza para el orden",
+		option4: "Tener un rol destacado en la política y sociedad",
+		respuesta: 4,
+		urlnxt: "/Romulo_Remo",
+	},
+];
